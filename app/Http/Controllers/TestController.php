146a6e5080e51532d79ba9d047112aefa43ca638
katsukawagami <joelai1988@gmail.com>
2020-05-06 13:47:16 +0800

    [Add]建立DB增刪修查頁面

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,11 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
-    public function test(){
+    public function test()
+    {
         phpinfo();
+    }
+    public function add()
+    {
+        echo '這是add頁面';
+    }
+    public function del()
+    {
+        echo '這是del頁面';
+    }
+    public function update()
+    {
+        echo '這是update頁面';
+    }
+    public function select()
+    {
+        echo '這是select頁面';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,13 @@ Route::get('/home/test', 'TestController@test');
 
 // 因為新版的laravel沒有影片中所教的Input的class 所以在google之後替換成request
 Route::get('/request', function () {
-    $requestTest = Request::get('id','空白');
+    $requestTest = Request::get('id', '空白');
     echo $requestTest;
+});
+
+Route::group(['prefix' => '/home/test'], function () {
+    Route::get('add', 'TestController@add');
+    Route::get('del', 'TestController@del');
+    Route::get('update', 'TestController@update');
+    Route::get('select', 'TestController@select');
 });
