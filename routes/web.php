5f21c82b57fd339256ababdd376ae2108bc6e443
katsukawagami <joelai1988@gmail.com>
2020-05-06 22:37:05 +0800

    [Update]修改首頁

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Request;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'TestController@test');
 
 Route::get('/home/test', 'TestController@test');
 
